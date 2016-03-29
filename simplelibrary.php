143f9cd4e819fa4d8cf7cb7aa72c40eb38b91dab
Blanuska <s.blanuska@gmail.com>
2016-03-29 22:04:03 +0200

    přepsané

---
+++ simplelibrary.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="cs-cz">
+        <head>
+  <meta charset="utf-8">
+  <title>Simple library</title>
+        </head>
+
+<body>
+<h1> Simple library <h1/>
+<form  method="post" >
+        ID publications:<input type="text" name="ID" size="50" autocomplete="on" /><br/>
+        ISBN:<input type="text" name="ISBN"/><br />
+        Title:<input type="text" name="Title" size="" /><br/>
+        NUmpages:<input type = "text" name = "Num pages"><br/>
+        Published: <input type="text" name="Publis">  <br/>   
+        Edition: <input type="text" name="Edition" autocomplete="on" /><br/>
+        Created_at: <input type ="text" name= "Created at"><br />
+        <input type="submit"/>
+        <button name="Vyprázdnit" type="reset" value="" <b>Vyprázdnit</b>
+                </button><br/>	
+</form>  
+
+</body>
+</html>
